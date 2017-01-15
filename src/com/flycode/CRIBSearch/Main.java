9237c09a23606d0a9a8a3a96bec7480f8d4550fe
ErickNK <eriknjiru73@gmail.com>
2017-01-15 08:53:49 +0300

    more changes

--- src/com/flycode/CRIBSearch/Main.java
+++ src/com/flycode/CRIBSearch/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("mainActivity.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/mainActivity.fxml"));
         Parent root = fxmlLoader.load();
         mainActivityController controller = fxmlLoader.getController();
         controller.initialize(padSql);
